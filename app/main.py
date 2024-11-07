f191c15d4e49c8665b21d7b9e198cf4dd9524410
jilvin Abraham <jilvinthomas@gmail.com>
2024-11-07 14:08:59 +0530

    Made changes in main.py to test webhook

--- app/main.py
+++ app/main.py
@@ -6,5 +6,5 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello, FastAPI in Docker! Updated"}
+    return {"message": "Hello, FastAPI in Docker! Testing webhook"}
 
